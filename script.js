382aa9c19b23a6c6b1b3821eaefa1c30a6690ef4
kjnb-0 <80792196+kjnb-0@users.noreply.github.com>
2021-07-24 12:26:50 -0700

    adding api links

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 //https://apidocs.covidactnow.org/data-definitions
 //Congrats, your new API key is 1848b424826d4a65a0322573e16402b2
 
+//state https://api.covidactnow.org/v2/state/{state}.json?apiKey=YOUR_KEY_HERE
+//county https://api.covidactnow.org/v2/county/{fips}.json?apiKey=YOUR_KEY_HERE
+//metros https://api.covidactnow.org/v2/cbsa/{cbsa_code}.json?apiKey=YOUR_KEY_HERE
+
