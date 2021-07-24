26fe20327a1a5d5e66acef32c53142baac09af6b
Proton-8 <r.a.shutter@gmail.com>
2021-07-24 13:38:16 -0700

    start of Yelp info

---
+++ yelp.js
@@ -0,0 +1,8 @@
+//YELP info ---
+//Client ID
+// aubh5OaIg5k2HVwTA5Cuwg
+
+// API Key
+
+// e2BwMzqg1HEbMbW-regclv9WYJ5419WanbkfH9dU5vbW03BjfF4ETOV8dEW1Ni21tBJc7k8XEJK6BQgl38xzLOknLazPc9O8sWjWhLMC2nMfPWdXkckPpALCO3f8YHYx
+//
