e0b879e0a69b2df2077fcd5a906af702835791a8
Proton-8 <r.a.shutter@gmail.com>
2021-07-27 21:28:53 -0700

    ron's add in

--- yelp.js
+++ yelp.js
@@ -6,3 +6,26 @@
 
 // e2BwMzqg1HEbMbW-regclv9WYJ5419WanbkfH9dU5vbW03BjfF4ETOV8dEW1Ni21tBJc7k8XEJK6BQgl38xzLOknLazPc9O8sWjWhLMC2nMfPWdXkckPpALCO3f8YHYx
 //
+
+const apiKey ="e2BwMzqg1HEbMbW-regclv9WYJ5419WanbkfH9dU5vbW03BjfF4ETOV8dEW1Ni21tBJc7k8XEJK6BQgl38xzLOknLazPc9O8sWjWhLMC2nMfPWdXkckPpALCO3f8YHYx"
+requestBizUrl = "https://api.yelp.com/v3/businesses/search?location=seattle&term=resturants&sort_by=rating&limit=5&"
+
+// .addEventListener('click', (btn) => {
+//     const city = document.getElementById('city').value;
+// })
+
+function api(){
+$.ajax({
+    url: requestBizUrl,
+    method: 'GET',
+    headers: {"Authorization": "Bearer " + apiKey, "Access-Control-Allow-Origin": "*"}
+   
+  })
+  .then(function (response) {
+    console.log('Ajax Reponse \n-------------');
+    console.log(response);
+        
+  });
+}
+api();
+
