bc0724751d2f0ebba936c319acf6bd6ddfb77eae
kjnb-0 <80792196+kjnb-0@users.noreply.github.com>
2021-07-30 13:35:49 -0700

    adding option/value full state name

--- assets/js/covidapi.js
+++ assets/js/covidapi.js
@@ -1,10 +1,5 @@
 //https://apidocs.covidactnow.org/data-definitions
 
-//by specific parameter
-//state https://api.covidactnow.org/v2/state/{state}.json?apiKey=1848b424826d4a65a0322573e16402b2
-//county https://api.covidactnow.org/v2/county/{fips}.json?apiKey=1848b424826d4a65a0322573e16402b2
-//metros https://api.covidactnow.org/v2/cbsa/{cbsa_code}.json?apiKey=1848b424826d4a65a0322573e16402b2
-
 apikey = "1848b424826d4a65a0322573e16402b2";
 requestStateUrl = "https://api.covidactnow.org/v2/states.json?apiKey=" + apikey;
 
@@ -14,22 +9,25 @@ $.ajax({
   method: "GET",
 }).then(function (response) {
   console.log(response);
-  //selection - when form submits, use that info to fetch specific state info 
-  //display that info on the page 
+  //loop through states for selection on homepage
   for (let i = 0; i < response.length; i++) {
     let stateName = response[i]["state"];
-    //change li to option value inside select element that will be made
-    let stateNameList = $("<li></li>").text(stateName)
-    //<option value="full state name">WA</option>
-    $("#search").append(stateNameList)
+
+   //getting full state name value
+    let urlResponse = response[i]["url"].split("/")
+    stateUrl = urlResponse[4].split("-")
+    stateNameUrl = stateUrl[0]
+    console.log(stateNameUrl)
+
+    //set option/value to state name and state abbreviation
+    let stateInfoList = $("<option></option>").text(stateName)
+    stateInfoList.val(stateNameUrl)
+    $("#search").append(stateInfoList)
+
   }
-
 });
-
     //placeholder for now - change to user selected
     let selectedState = "WA";
-
-
     $.ajax({
       url:
         "https://api.covidactnow.org/v2/state/" +
@@ -40,21 +38,6 @@ $.ajax({
     }).then(function (response) {
       console.log(response);
       console.log(selectedState);
-
-      let json = response["url"]
-      let obj = JSON.stringify(json)
-      console.log(obj)
-      let parse = JSON.parse(obj)
-      console.log(parse)
-
-      var urlresponse = json.split("/")
-      console.log(urlresponse)
-      console.log(urlresponse[4])
-      stateurl = urlresponse[4]
-      statenameurl = stateurl.split("-")
-      console.log(statenameurl)
-
-      
 
       let riskLevel = "Risk Level (0=low,1=medium,2=high,3=very high,4=severe): " + response["riskLevels"]["overall"];
       let riskLevelList = $("<p></p>").text(riskLevel)
