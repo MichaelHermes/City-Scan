419ec966459a967214c8b2b9f737446d2407f05b
kjnb-0 <80792196+kjnb-0@users.noreply.github.com>
2021-07-31 10:15:33 -0700

    updating script.js file with main state api call, covidapi is single state api call

--- assets/js/covidapi.js
+++ assets/js/covidapi.js
@@ -1,31 +1,7 @@
 //https://apidocs.covidactnow.org/data-definitions
 
 apikey = "1848b424826d4a65a0322573e16402b2";
-requestStateUrl = "https://api.covidactnow.org/v2/states.json?apiKey=" + apikey;
 
-//data for all states
-$.ajax({
-  url: requestStateUrl,
-  method: "GET",
-}).then(function (response) {
-  console.log(response);
-  //loop through states for selection on homepage
-  for (let i = 0; i < response.length; i++) {
-    let stateName = response[i]["state"];
-
-   //getting full state name value
-    let urlResponse = response[i]["url"].split("/")
-    stateUrl = urlResponse[4].split("-")
-    stateNameUrl = stateUrl[0]
-    console.log(stateNameUrl)
-
-    //set option/value to state name and state abbreviation
-    let stateInfoList = $("<option></option>").text(stateName)
-    stateInfoList.val(stateNameUrl)
-    $("#search").append(stateInfoList)
-
-  }
-});
     //placeholder for now - change to user selected
     let selectedState = "WA";
     $.ajax({
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,26 @@
-//https://apidocs.covidactnow.org/data-definitions
-//Congrats, your new API key is 1848b424826d4a65a0322573e16402b2
+apikey = "1848b424826d4a65a0322573e16402b2";
+requestStateUrl = "https://api.covidactnow.org/v2/states.json?apiKey=" + apikey;
 
-//list of all data by category
-// https://api.covidactnow.org/v2/states.json?apiKey=1848b424826d4a65a0322573e16402b2
-// https://api.covidactnow.org/v2/counties.json?apiKey=1848b424826d4a65a0322573e16402b2
-// https://api.covidactnow.org/v2/cbsas.json?apiKey=1848b424826d4a65a0322573e16402b2
-
-//by specific parameter
-//state https://api.covidactnow.org/v2/state/{state}.json?apiKey=1848b424826d4a65a0322573e16402b2
-//county https://api.covidactnow.org/v2/county/{fips}.json?apiKey=1848b424826d4a65a0322573e16402b2
-//metros https://api.covidactnow.org/v2/cbsa/{cbsa_code}.json?apiKey=1848b424826d4a65a0322573e16402b2
-
-requestCountyUrl = "https://api.covidactnow.org/v2/counties.json?apiKey=1848b424826d4a65a0322573e16402b2"
-
-requestMetrosUrl = "https://api.covidactnow.org/v2/cbsas.json?apiKey=1848b424826d4a65a0322573e16402b2"
-
-
+//data for all states
 $.ajax({
-    url: requestCountyUrl,
-    method: 'GET',
-  }).then(function (response) {
-    console.log('Ajax Reponse \n-------------');
-    console.log(response);
-        //targeting specific place
-        console.log(response[3049]);
-        console.log(response[3049]["county"]);       
-        console.log(response[3049]["actuals"]);        
-        console.log(response[3049]["riskLevels"]);
-  });
+  url: requestStateUrl,
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+  //loop through states for selection on homepage
+  for (let i = 0; i < response.length; i++) {
+    let stateName = response[i]["state"];
 
-  //locationID is city name - 
-  //documentation https://apidocs.covidactnow.org/api/#tag/CBSA-Data/paths/~1cbsas.json?apiKey={apiKey}/get
-  //and github explanation https://github.com/covidatlas/li/blob/master/docs/reports-v1.md#general-notes
-  $.ajax({
-    url: requestMetrosUrl,
-    method: 'GET',
-  }).then(function (response) {
-    console.log('Ajax Reponse \n-------------');
-    console.log(response);
+   //getting full state name value
+    let urlResponse = response[i]["url"].split("/")
+    stateUrl = urlResponse[4].split("-")
+    stateNameUrl = stateUrl[0]
+    console.log(stateNameUrl)
 
-  });
+    //set option/value to state name and state abbreviation
+    let stateInfoList = $("<option></option>").text(stateName)
+    stateInfoList.val(stateNameUrl)
+    $("#search").append(stateInfoList)
 
+  }
+});
