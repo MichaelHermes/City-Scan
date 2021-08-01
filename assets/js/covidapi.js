740b49b014fe447398b7c072dfac3f163948d546
kjnb-0 <80792196+kjnb-0@users.noreply.github.com>
2021-08-01 11:03:49 -0700

    working on resubmit function

--- assets/js/covidapi.js
+++ assets/js/covidapi.js
@@ -1,14 +1,21 @@
-//https://apidocs.covidactnow.org/data-definitions
-
 apikey = "1848b424826d4a65a0322573e16402b2";
 
-    //placeholder for now - change to user selected
-
+//get user input from localstorage for state - use state variable to call API
 let obj = JSON.parse(localStorage.getItem("City Search Criteria"))
-
 let selectedState = obj["State-Abbreviation"]
-console.log(selectedState);
+let selectedStateName = obj["State-Name"]
 
+statelookup(selectedStateName);
+
+function statelookup() {
+  //format api response to display nicely in HTML - from https://masteringjs.io/tutorials/fundamentals/capitalize-first-letter
+let str = selectedStateName.replace(/\_/g, ' ');
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+let caps = str.split(' ').map(capitalize).join(' ');
+
+$("#state-name").append(caps)
     $.ajax({
       url:
         "https://api.covidactnow.org/v2/state/" +
@@ -32,9 +39,8 @@ console.log(selectedState);
       $("#perc-vax").append(vaccinatedPercentList)
       
       let moreInfoLink = response["url"]
-      let moreInfo = "For more information visit " + $("<a></a>").attr("href",moreInfoLink).text(moreInfoLink)
-      let moreInfoList = $("<p></p>").text(moreInfo)
-      $("#state-name").append(moreInfoList)
+      let moreInfo = $("<a></a>").attr("href",moreInfoLink).text(moreInfoLink)
+      $("#more-info").append(moreInfo)
 
       if (riskLevel === 0) {
         riskLevelList.addClass("low") 
@@ -48,7 +54,7 @@ console.log(selectedState);
         riskLevelList.addClass("severe")
       }
     });
-
+  }
 
 
 requestStateUrl = "https://api.covidactnow.org/v2/states.json?apiKey=" + apikey;
@@ -58,7 +64,6 @@ $.ajax({
   url: requestStateUrl,
   method: "GET",
 }).then(function (response) {
-  console.log(response);
   //loop through states for selection on searchbar
   for (let i = 0; i < response.length; i++) {
     let stateName = response[i]["state"];
@@ -66,11 +71,69 @@ $.ajax({
     let urlResponse = response[i]["url"].split("/")
     stateUrl = urlResponse[4].split("-")
     stateNameUrl = stateUrl[0]
-    // console.log(stateNameUrl)
-
+    
     //set option/value to state name and state abbreviation
     let stateInfoList = $("<option></option>").text(stateName)
     stateInfoList.val(stateNameUrl)
     $("#state-list").append(stateInfoList)
   }
 });
+
+$("#search").on("submit", function(event) {
+  event.preventDefault();
+  let stateSelect = $("#state-list option:checked").text()
+  $("#state-name").innerHTML = ""
+  $("#risk-level").innerHTML = ""
+  $("#daily-case").innerHTML = ""
+  $("#perc-vax").innerHTML = ""
+  $("#more-info").innerHTML = ""
+  console.log(stateSelect)
+  function formstatelookup(){
+    let str = stateSelect.replace(/\_/g, ' ');
+    function capitalize(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+    let caps = str.split(' ').map(capitalize).join(' ');
+    $("#state-name").append(caps)
+        $.ajax({
+          url:
+            "https://api.covidactnow.org/v2/state/" +
+            stateSelect +
+            ".json?apiKey=" +
+            apikey,
+          method: "GET",
+        }).then(function (response) {
+          console.log(response)   
+          
+          
+          let riskLevel = response["riskLevels"]["overall"];
+          let riskLevelList = $("<p></p>").text(riskLevel)
+          $("#risk-level").append(riskLevelList)
+          
+          let newCases = response["actuals"]["newCases"];
+          let newCasesList = $("<p></p>").text(newCases)
+          $("#daily-case").append(newCasesList)
+          
+          let vaccinatedPercent = (response["metrics"]["vaccinationsCompletedRatio"] * 100).toFixed(2) + "%"
+          let vaccinatedPercentList = $("<p></p>").text(vaccinatedPercent)
+          $("#perc-vax").append(vaccinatedPercentList)
+          
+          let moreInfoLink = response["url"]
+          let moreInfo = $("<a></a>").attr("href",moreInfoLink).text(moreInfoLink)
+          $("#more-info").append(moreInfo)
+    
+          if (riskLevel === 0) {
+            riskLevelList.addClass("low") 
+          } else if (riskLevel === 1) {
+            riskLevelList.addClass("medium")
+          } else if (riskLevel === 2) {
+            riskLevelList.addClass ("high")
+          } else if (riskLevel === 3) {
+            riskLevelList.addClass("very-high")
+          } else {
+            riskLevelList.addClass("severe")
+          }
+        });
+      }
+      formstatelookup();
+  })
