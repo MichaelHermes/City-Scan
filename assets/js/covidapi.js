488009961189e2d8a17c794cc147999f07c975ff
kjnb-0 <80792196+kjnb-0@users.noreply.github.com>
2021-08-01 11:43:51 -0700

    clears page on new search, loads new search values

--- assets/js/covidapi.js
+++ assets/js/covidapi.js
@@ -1,61 +1,62 @@
 apikey = "1848b424826d4a65a0322573e16402b2";
 
 //get user input from localstorage for state - use state variable to call API
-let obj = JSON.parse(localStorage.getItem("City Search Criteria"))
-let selectedState = obj["State-Abbreviation"]
-let selectedStateName = obj["State-Name"]
+let obj = JSON.parse(localStorage.getItem("City Search Criteria"));
+let selectedState = obj["State-Abbreviation"];
+let selectedStateName = obj["State-Name"];
 
 statelookup(selectedStateName);
 
 function statelookup() {
   //format api response to display nicely in HTML - from https://masteringjs.io/tutorials/fundamentals/capitalize-first-letter
-let str = selectedStateName.replace(/\_/g, ' ');
-function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-let caps = str.split(' ').map(capitalize).join(' ');
-
-$("#state-name").append(caps)
-    $.ajax({
-      url:
-        "https://api.covidactnow.org/v2/state/" +
-        selectedState +
-        ".json?apiKey=" +
-        apikey,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response);
-
-      let riskLevel = response["riskLevels"]["overall"];
-      let riskLevelList = $("<p></p>").text(riskLevel)
-      $("#risk-level").append(riskLevelList)
-      
-      let newCases = response["actuals"]["newCases"];
-      let newCasesList = $("<p></p>").text(newCases)
-      $("#daily-case").append(newCasesList)
-      
-      let vaccinatedPercent = (response["metrics"]["vaccinationsCompletedRatio"] * 100).toFixed(2) + "%"
-      let vaccinatedPercentList = $("<p></p>").text(vaccinatedPercent)
-      $("#perc-vax").append(vaccinatedPercentList)
-      
-      let moreInfoLink = response["url"]
-      let moreInfo = $("<a></a>").attr("href",moreInfoLink).text(moreInfoLink)
-      $("#more-info").append(moreInfo)
-
-      if (riskLevel === 0) {
-        riskLevelList.addClass("low") 
-      } else if (riskLevel === 1) {
-        riskLevelList.addClass("medium")
-      } else if (riskLevel === 2) {
-        riskLevelList.addClass ("high")
-      } else if (riskLevel === 3) {
-        riskLevelList.addClass("very-high")
-      } else {
-        riskLevelList.addClass("severe")
-      }
-    });
+  let str = selectedStateName.replace(/\_/g, " ");
+  function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
   }
+  let caps = str.split(" ").map(capitalize).join(" ");
+  $("#state-name").html(caps);
 
+  $.ajax({
+    url:
+      "https://api.covidactnow.org/v2/state/" +
+      selectedState +
+      ".json?apiKey=" +
+      apikey,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+
+    let riskLevel = response["riskLevels"]["overall"];
+    let riskLevelList = $("<p></p>").text(riskLevel);
+    $("#risk-level").html(riskLevelList);
+
+    let newCases = response["actuals"]["newCases"];
+    let newCasesList = $("<p></p>").text(newCases);
+    $("#daily-case").html(newCasesList);
+
+    let vaccinatedPercent =
+      (response["metrics"]["vaccinationsCompletedRatio"] * 100).toFixed(2) +
+      "%";
+    let vaccinatedPercentList = $("<p></p>").text(vaccinatedPercent);
+    $("#perc-vax").html(vaccinatedPercentList);
+
+    let moreInfoLink = response["url"];
+    let moreInfo = $("<a></a>").attr("href", moreInfoLink).text(moreInfoLink);
+    $("#more-info").html(moreInfo);
+
+    if (riskLevel === 0) {
+      riskLevelList.addClass("low");
+    } else if (riskLevel === 1) {
+      riskLevelList.addClass("medium");
+    } else if (riskLevel === 2) {
+      riskLevelList.addClass("high");
+    } else if (riskLevel === 3) {
+      riskLevelList.addClass("very-high");
+    } else {
+      riskLevelList.addClass("severe");
+    }
+  });
+}
 
 requestStateUrl = "https://api.covidactnow.org/v2/states.json?apiKey=" + apikey;
 
@@ -68,72 +69,77 @@ $.ajax({
   for (let i = 0; i < response.length; i++) {
     let stateName = response[i]["state"];
 
-    let urlResponse = response[i]["url"].split("/")
-    stateUrl = urlResponse[4].split("-")
-    stateNameUrl = stateUrl[0]
-    
-    //set option/value to state name and state abbreviation
-    let stateInfoList = $("<option></option>").text(stateName)
-    stateInfoList.val(stateNameUrl)
-    $("#state-list").append(stateInfoList)
+    let urlResponse = response[i]["url"].split("/");
+    stateUrl = urlResponse[4].split("-");
+    stateNameUrl = stateUrl[0];
+
+    let stateInfoList = $("<option></option>").text(stateName);
+    stateInfoList.val(stateNameUrl);
+    $("#state-list").append(stateInfoList);
   }
 });
 
-$("#search").on("submit", function(event) {
+$("#search").on("submit", function (event) {
   event.preventDefault();
-  let stateSelect = $("#state-list option:checked").text()
-  $("#state-name").innerHTML = ""
-  $("#risk-level").innerHTML = ""
-  $("#daily-case").innerHTML = ""
-  $("#perc-vax").innerHTML = ""
-  $("#more-info").innerHTML = ""
-  console.log(stateSelect)
-  function formstatelookup(){
-    let str = stateSelect.replace(/\_/g, ' ');
-    function capitalize(str) {
-      return str.charAt(0).toUpperCase() + str.slice(1);
-    }
-    let caps = str.split(' ').map(capitalize).join(' ');
-    $("#state-name").append(caps)
-        $.ajax({
-          url:
-            "https://api.covidactnow.org/v2/state/" +
-            stateSelect +
-            ".json?apiKey=" +
-            apikey,
-          method: "GET",
-        }).then(function (response) {
-          console.log(response)   
-          
-          
-          let riskLevel = response["riskLevels"]["overall"];
-          let riskLevelList = $("<p></p>").text(riskLevel)
-          $("#risk-level").append(riskLevelList)
-          
-          let newCases = response["actuals"]["newCases"];
-          let newCasesList = $("<p></p>").text(newCases)
-          $("#daily-case").append(newCasesList)
-          
-          let vaccinatedPercent = (response["metrics"]["vaccinationsCompletedRatio"] * 100).toFixed(2) + "%"
-          let vaccinatedPercentList = $("<p></p>").text(vaccinatedPercent)
-          $("#perc-vax").append(vaccinatedPercentList)
-          
-          let moreInfoLink = response["url"]
-          let moreInfo = $("<a></a>").attr("href",moreInfoLink).text(moreInfoLink)
-          $("#more-info").append(moreInfo)
-    
-          if (riskLevel === 0) {
-            riskLevelList.addClass("low") 
-          } else if (riskLevel === 1) {
-            riskLevelList.addClass("medium")
-          } else if (riskLevel === 2) {
-            riskLevelList.addClass ("high")
-          } else if (riskLevel === 3) {
-            riskLevelList.addClass("very-high")
-          } else {
-            riskLevelList.addClass("severe")
-          }
-        });
+  //new state selected from dropdown menu
+  let stateSelect = $("#state-list option:checked").text();
+
+  function formstatelookup() {
+    $.ajax({
+      url:
+        "https://api.covidactnow.org/v2/state/" +
+        stateSelect +
+        ".json?apiKey=" +
+        apikey,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      //full state name
+      let urlResponse = response["url"].split("/");
+      stateUrl = urlResponse[4].split("-");
+      stateNameUrl = stateUrl[0];
+      let str = stateNameUrl.replace(/\_/g, " ");
+      function capitalize(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
       }
-      formstatelookup();
-  })
+      let caps = str.split(" ").map(capitalize).join(" ");
+      $("#state-name").innerHTML = "";
+      $("#state-name").html(caps);
+
+      let riskLevel = response["riskLevels"]["overall"];
+      let riskLevelList = $("<p></p>").text(riskLevel);
+      $("#risk-level").innerHTML = "";
+      $("#risk-level").html(riskLevelList);
+
+      let newCases = response["actuals"]["newCases"];
+      let newCasesList = $("<p></p>").text(newCases);
+      $("#daily-case").innerHTML = "";
+      $("#daily-case").html(newCasesList);
+
+      let vaccinatedPercent =
+        (response["metrics"]["vaccinationsCompletedRatio"] * 100).toFixed(2) +
+        "%";
+      let vaccinatedPercentList = $("<p></p>").text(vaccinatedPercent);
+      $("#perc-vax").innerHTML = "";
+      $("#perc-vax").html(vaccinatedPercentList);
+
+      let moreInfoLink = response["url"];
+      let moreInfo = $("<a></a>").attr("href", moreInfoLink).text(moreInfoLink);
+      $("#more-info").innerHTML = "";
+      $("#more-info").html(moreInfo);
+
+      if (riskLevel === 0) {
+        riskLevelList.addClass("low");
+      } else if (riskLevel === 1) {
+        riskLevelList.addClass("medium");
+      } else if (riskLevel === 2) {
+        riskLevelList.addClass("high");
+      } else if (riskLevel === 3) {
+        riskLevelList.addClass("very-high");
+      } else {
+        riskLevelList.addClass("severe");
+      }
+    });
+  }
+  formstatelookup();
+});
